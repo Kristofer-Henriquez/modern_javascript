811e6013fb666038f7af9927d10b7499607639a7
Kristofer <kristoferhenriquez@gmail.com>
2020-07-30 20:41:35 -0500

    installed webpack and converted my index.js file while testing another module

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+var moment = require("moment");
+
+var tinycolor = require("tinycolor2");
+
+
 console.log("hello from javascript!");
 
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+console.log(tinycolor("#000"));
